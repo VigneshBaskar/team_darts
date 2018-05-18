f48962641a08b9e545269a49267259ba7a3c53ec
Vignesh Baskar <vignesh.sbaskaran@gmail.com>
2018-05-18 13:24:21 +0200

    Light GBM initial baseline

--- credit-risk/DataExploration.py
+++ credit-risk/DataExploration.py
@@ -4,6 +4,12 @@
 # <codecell>
 
 import pandas as pd
+import numpy as np
+
+# <codecell>
+
+from sklearn.model_selection import train_test_split
+import lightgbm as lgb
 
 # <codecell>
 
@@ -16,30 +22,64 @@ pd.options.display.max_rows = len(application_train.columns)
 
 # <codecell>
 
-application_train.head()
+application_features = [column for column in application_train.columns.tolist() if column!='SK_ID_CURR' and column!='TARGET']
 
 # <codecell>
 
-categorical_features = ['NAME_CONTRACT_TYPE', 'CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY', 'NAME_TYPE_SUITE', 'NAME_INCOME_TYPE',
-                       'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS', 'NAME_HOUSING_TYPE', 'FLAG_MOBIL', 'FLAG_EMP_PHONE',
-                       'FLAG_WORK_PHONE', 'FLAG_CONT_MOBILE', 'FLAG_PHONE', 'FLAG_EMAIL', 'OCCUPATION_TYPE', 'REGION_RATING_CLIENT',
-                       'REGION_RATING_CLIENT_W_CITY', 'WEEKDAY_APPR_PROCESS_START', 'REG_REGION_NOT_LIVE_REGION',
-                       'REG_REGION_NOT_WORK_REGION','LIVE_REGION_NOT_WORK_REGION','REG_CITY_NOT_LIVE_CITY','REG_CITY_NOT_WORK_CITY',
-                       'LIVE_CITY_NOT_WORK_CITY', 'ORGANIZATION_TYPE']
+categorical_features = []
+continuous_features = []
+for application_feature in application_features:
+    if (application_train.dtypes[application_feature]=='object') or ( application_train.dtypes[application_feature]=='int64' and len(application_train[application_feature].unique())<20):
+        categorical_features.append(application_feature)
+    else:
+        continuous_features.append(application_feature)
 
 # <codecell>
 
-continuous_features = ['CNT_CHILDREN','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY','AMT_GOODS_PRICE','REGION_POPULATION_RELATIVE',
-                      'REGION_POPULATION_RELATIVE', 'DAYS_BIRTH','DAYS_EMPLOYED', 'DAYS_REGISTRATION','OWN_CAR_AGE',
-                      'CNT_FAM_MEMBERS','HOUR_APPR_PROCESS_START','EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3',
-                      'APARTMENTS_AVG','BASEMENTAREA_AVG','YEARS_BEGINEXPLUATATION_AVG']
+# Replace Unknowns with 'unk' for categorical features and '0' for continuos features
+application_train[categorical_features] = application_train[categorical_features].fillna(value='unk')
+application_train[continuous_features] = application_train[continuous_features].fillna(value=0)
 
 # <codecell>
 
-for categorical_feature in categorical_features:
-    print(categorical_feature, len(application_train[categorical_feature].unique()), application_train.dtypes[categorical_feature])
+for column in categorical_features:
+    application_train[column] = application_train[column].astype('category')
 
 # <codecell>
 
-for continuous_feature in continuous_features:
-    print(continuous_feature, len(application_train[continuous_feature].unique()), application_train.dtypes[continuous_feature])
+input_columns = application_train.columns
+input_columns = input_columns[input_columns != 'TARGET']
+target_column = 'TARGET'
+
+X = application_train[input_columns]
+y = application_train[target_column]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1234)
+
+lgb_train = lgb.Dataset(data=X_train, label=y_train)
+lgb_eval = lgb.Dataset(data=X_test, label=y_test)
+
+params = {
+        'task': 'train',
+        'boosting_type': 'gbdt',
+        'objective': 'binary',
+        'metric': {'auc'},
+        'learning_rate': 0.1,
+        'num_leaves': 23,
+        'min_data_in_leaf': 1,
+        'num_iteration': 200,
+        'verbose': 0
+}
+
+# train
+gbm = lgb.train(params,
+            lgb_train,
+            num_boost_round=50,
+            valid_sets=lgb_eval,
+            early_stopping_rounds=10)
+
+# <codecell>
+
+prob = 0.2
+
+print(np.sum(gbm.predict(X_test)[np.asarray(y_test==1)]>prob)/np.sum(y_test==1))
+print(np.sum(gbm.predict(X_test)[np.asarray(y_test==0)]<prob)/np.sum(y_test==0))
