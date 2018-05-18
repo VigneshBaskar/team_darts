026904b99f8004d6ceb9531ec4bcc8c347aba948
Vignesh Baskar <vignesh.sbaskaran@gmail.com>
2018-05-18 13:23:50 +0200

    Added toxic comments scripts

--- toxic-comment/main.py
+++ toxic-comment/main.py
@@ -3,4 +3,21 @@
 
 # <codecell>
 
-import os
+import pandas as pd
+import numpy as np
+
+# <codecell>
+
+train = pd.read_csv('Data/train.csv')
+test = pd.read_csv('Data/test.csv')
+subm = pd.read_csv('Data/sample_submission.csv')
+
+# <codecell>
+
+# Adding a label clean
+label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+train['clean'] = train[label_cols].max(axis=1)
+
+# Replacing empty cells with text unknown
+train["comment_text"].fillna("unknown", inplace=True)
+test["comment_text"].fillna("unknown", inplace=True)
